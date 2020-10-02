dd4064a89ae8543660bc506b51ab7800e47c5027
Rashil Gandhi <rashil2000@gmail.com>
2020-10-03 02:34:06 +0530

    Move links to bottom, like other pages

--- pages/index.js
+++ pages/index.js
@@ -46,10 +46,14 @@ export default function Home() {
         <table id="no-border" style={{ width: "100%" }}>
           <tbody>
             <tr>
-              <td id="no-border" style={{ width: "25%" }}><a href="resume.pdf"><p className="author"><i>Résumé</i></p></a></td>
-              <td id="no-border" style={{ width: "25%" }}><Link href="blogs"><a><p className="author"><i>Blogs</i></p></a></Link></td>
-              <td id="no-border" style={{ width: "25%" }}><Link href="projects"><a><p className="author"><i>Projects</i></p></a></Link></td>
-              <td id="no-border" style={{ width: "25%" }}><Link href="admin"><a><p className="author"><i>Admin</i></p></a></Link></td>
+              <td id="no-border"><a target="_blank" rel="noopener" href="https://github.com/rashil2000"><img id="social-icon" src="images/github.svg" alt="GitHub" /></a></td>
+              <td id="no-border"><a target="_blank" rel="noopener" href="https://gitlab.com/rashil2000"><img id="social-icon" src="images/gitlab.svg" alt="GitLab" /></a></td>
+              <td id="no-border"><a target="_blank" rel="noopener" href="https://twitter.com/rashil2000"><img id="social-icon" src="images/twitter.svg" alt="Twitter" /></a></td>
+              <td id="no-border"><a target="_blank" rel="noopener" href="https://linkedin.com/in/rashil2000"><img id="social-icon" src="images/linkedin.svg" alt="LinkedIn" /></a></td>
+              <td id="no-border"><a target="_blank" rel="noopener" href="https://facebook.com/rashil2000"><img id="social-icon" src="images/facebook.svg" alt="Facebook" /></a></td>
+              <td id="no-border"><a target="_blank" rel="noopener" href="https://instagram.com/rashil_2000"><img id="social-icon" src="images/instagram.svg" alt="Instagram" /></a></td>
+              <td id="no-border"><a target="_blank" rel="noopener" href="https://reddit.com/u/rashil2000"><img id="social-icon" src="images/reddit.svg" alt="Reddit" /></a></td>
+              <td id="no-border"><a target="_blank" rel="noopener" href="https://youtube.com/rashilgandhi2000"><img id="social-icon" src="images/youtube.svg" alt="YouTube" /></a></td>
             </tr>
           </tbody>
         </table>
@@ -60,14 +64,10 @@ export default function Home() {
         <table id="no-border" style={{ width: "100%" }}>
           <tbody>
             <tr>
-              <td id="no-border"><a target="_blank" rel="noopener" href="https://github.com/rashil2000"><img id="social-icon" src="images/github.svg" alt="GitHub" /></a></td>
-              <td id="no-border"><a target="_blank" rel="noopener" href="https://gitlab.com/rashil2000"><img id="social-icon" src="images/gitlab.svg" alt="GitLab" /></a></td>
-              <td id="no-border"><a target="_blank" rel="noopener" href="https://twitter.com/rashil2000"><img id="social-icon" src="images/twitter.svg" alt="Twitter" /></a></td>
-              <td id="no-border"><a target="_blank" rel="noopener" href="https://linkedin.com/in/rashil2000"><img id="social-icon" src="images/linkedin.svg" alt="LinkedIn" /></a></td>
-              <td id="no-border"><a target="_blank" rel="noopener" href="https://facebook.com/rashil2000"><img id="social-icon" src="images/facebook.svg" alt="Facebook" /></a></td>
-              <td id="no-border"><a target="_blank" rel="noopener" href="https://instagram.com/rashil_2000"><img id="social-icon" src="images/instagram.svg" alt="Instagram" /></a></td>
-              <td id="no-border"><a target="_blank" rel="noopener" href="https://reddit.com/u/rashil2000"><img id="social-icon" src="images/reddit.svg" alt="Reddit" /></a></td>
-              <td id="no-border"><a target="_blank" rel="noopener" href="https://youtube.com/rashilgandhi2000"><img id="social-icon" src="images/youtube.svg" alt="YouTube" /></a></td>
+              <td id="no-border" style={{ width: "25%" }}><a href="resume.pdf"><p className="author" style={{ fontVariantCaps: "all-small-caps" }}>Résumé</p></a></td>
+              <td id="no-border" style={{ width: "25%" }}><Link href="blogs"><a><p className="author" style={{ fontVariantCaps: "all-small-caps" }}>Blogs</p></a></Link></td>
+              <td id="no-border" style={{ width: "25%" }}><Link href="projects"><a><p className="author" style={{ fontVariantCaps: "all-small-caps" }}>Projects</p></a></Link></td>
+              <td id="no-border" style={{ width: "25%" }}><Link href="admin"><a><p className="author" style={{ fontVariantCaps: "all-small-caps" }}>Admin</p></a></Link></td>
             </tr>
           </tbody>
         </table>
